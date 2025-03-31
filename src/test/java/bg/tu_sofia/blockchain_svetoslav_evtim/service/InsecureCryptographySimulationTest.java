ab55517bcef4671b417fbf52d856003bd916655f
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2025-03-31 21:00:25 +0300

    add test

--- src/test/java/bg/tu_sofia/blockchain_svetoslav_evtim/service/InsecureCryptographySimulationTest.java
+++ src/test/java/bg/tu_sofia/blockchain_svetoslav_evtim/service/InsecureCryptographySimulationTest.java
@@ -2,7 +2,14 @@ package bg.tu_sofia.blockchain_svetoslav_evtim.service;
 
 import org.junit.jupiter.api.Test;
 
+import javax.crypto.KeyGenerator;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class InsecureCryptographySimulationTest {
 
@@ -11,5 +18,11 @@ class InsecureCryptographySimulationTest {
         InsecureCryptographySimulation simulation = new InsecureCryptographySimulation();
         String encryptedData = simulation.encrypt("SensitiveData");
         assertThat(encryptedData).isNotNull().isNotEmpty();
+    }
+
+    @Test
+    public void testEncryptException() {
+        InsecureCryptographySimulation simulation = new InsecureCryptographySimulation();
+        assertThrows(GeneralSecurityException.class, () -> simulation.encrypt(null));
     }
 }
