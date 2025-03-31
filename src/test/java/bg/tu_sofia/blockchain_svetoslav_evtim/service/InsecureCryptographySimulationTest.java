765630921b106fdd855f3523b3149e5b1223ba4f
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2025-03-31 21:02:29 +0300

    remove imports

--- src/test/java/bg/tu_sofia/blockchain_svetoslav_evtim/service/InsecureCryptographySimulationTest.java
+++ src/test/java/bg/tu_sofia/blockchain_svetoslav_evtim/service/InsecureCryptographySimulationTest.java
@@ -2,14 +2,10 @@ package bg.tu_sofia.blockchain_svetoslav_evtim.service;
 
 import org.junit.jupiter.api.Test;
 
-import javax.crypto.KeyGenerator;
 import java.security.GeneralSecurityException;
-import java.security.NoSuchAlgorithmException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class InsecureCryptographySimulationTest {
 
